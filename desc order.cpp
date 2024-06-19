274d35fd6fb81706bdcdf911327d61467ed809e5
Heba_Mustafa🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-19 14:47:51 +0300

    Add files via upload

---
+++ desc order.cpp
@@ -0,0 +1,17 @@
+﻿/* yildiz
+*/
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+   
+	vector<int> v = { 5,2,4,7,8,1 };
+	sort(v.rbegin(), v.rend());
+	for (auto it : v)
+	{
+		cout << it << "\n";
+	}
+	
+}
+
